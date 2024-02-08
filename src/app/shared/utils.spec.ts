32061d70ac9e545efde22b43208921807ccba2c0
Christian Badura <christian.badura@capgemini.com>
2024-02-08 11:41:00 +0100

    feat: finish utils

--- src/app/shared/utils.spec.ts
+++ src/app/shared/utils.spec.ts
@@ -1,8 +1,69 @@
-import { limitText } from './utils'
+import { SelectItem } from 'primeng/api'
+
+import { limitText, dropDownSortItemsByLabel, dropDownGetLabelByValue, sortByLocale } from './utils'
 
 describe('utils', () => {
-  it('should limit text if text too long', () => {
-    const result = limitText('textData', 4)
-    expect(result).toBe('text...')
+  describe('limitText', () => {
+    it('should limit text if text too long', () => {
+      const result = limitText('textData', 4)
+      expect(result).toBe('text...')
+    })
+
+    it('should limit text if text too long', () => {
+      const result = limitText('textData', 10)
+      expect(result).toBe('textData')
+    })
+
+    it('return empty string if no text', () => {
+      const result = limitText('', 4)
+      expect(result).toBe('')
+    })
+  })
+
+  describe('dropDownSortItemsByLabel', () => {
+    it('should correctly sort items by label', () => {
+      const items: SelectItem[] = [
+        { label: 'label2', value: 2 },
+        { label: 'label1', value: 1 }
+      ]
+
+      const sortedItems = items.sort(dropDownSortItemsByLabel)
+
+      expect(sortedItems[0].label).toEqual('label1')
+    })
+    it("should treat falsy values for SelectItem.label as ''", () => {
+      const items: SelectItem[] = [
+        { label: undefined, value: 1 },
+        { label: undefined, value: 2 },
+        { label: 'label1', value: 2 }
+      ]
+
+      const sortedItems = items.sort(dropDownSortItemsByLabel)
+
+      expect(sortedItems[0].label).toEqual(undefined)
+    })
+  })
+
+  describe('dropDownGetLabelByValue', () => {
+    it('should return the label corresponding to the value', () => {
+      const items: SelectItem[] = [
+        { label: 'label2', value: 2 },
+        { label: 'label1', value: 1 }
+      ]
+
+      const result = dropDownGetLabelByValue(items, '1')
+
+      expect(result).toEqual('label1')
+    })
+  })
+
+  describe('sortByLocale', () => {
+    it('should sort strings based on locale', () => {
+      const strings: string[] = ['str2', 'str1']
+
+      const sortedStrings = strings.sort(sortByLocale)
+
+      expect(sortedStrings[0]).toEqual('str1')
+    })
   })
 })
