3fd5702ce61ebd629063473558ae1bc52c629338
Henry Taeschner <henry.taeschner@capgemini.com>
2024-04-16 11:45:17 +0200

    fix: use get-by-id to get apps in detail pages

--- src/app/product-store/app-detail/app-detail.component.ts
+++ src/app/product-store/app-detail/app-detail.component.ts
@@ -10,14 +10,14 @@ import { dropDownSortItemsByLabel, convertToUniqueStringArray } from 'src/app/sh
 import {
   CreateMicrofrontendRequest,
   CreateMicroserviceRequest,
-  GetMicrofrontendByAppIdRequestParams,
+  GetMicrofrontendRequestParams,
+  GetMicroserviceRequestParams,
   MicrofrontendsAPIService,
-  Microservice,
-  Microfrontend,
-  UpdateMicrofrontendRequest,
-  UpdateMicroserviceRequest,
   MicroservicesAPIService,
-  GetMicroserviceByAppIdRequestParams
+  Microfrontend,
+  Microservice,
+  UpdateMicrofrontendRequest,
+  UpdateMicroserviceRequest
 } from 'src/app/shared/generated'
 
 import { AppAbstract, ChangeMode } from '../app-search/app-search.component'
@@ -142,7 +142,7 @@ export class AppDetailComponent implements OnChanges {
   public getMfe() {
     this.loading = true
     this.mfeApi
-      .getMicrofrontendByAppId({ appId: this.appAbstract?.appId } as GetMicrofrontendByAppIdRequestParams)
+      .getMicrofrontend({ id: this.appAbstract?.id ?? '' } as GetMicrofrontendRequestParams)
       .pipe(finalize(() => (this.loading = false)))
       .subscribe({
         next: (data: any) => {
@@ -165,7 +165,7 @@ export class AppDetailComponent implements OnChanges {
   public getMs() {
     this.loading = true
     this.msApi
-      .getMicroserviceByAppId({ appId: this.appAbstract?.appId } as GetMicroserviceByAppIdRequestParams)
+      .getMicroservice({ id: this.appAbstract?.id } as GetMicroserviceRequestParams)
       .pipe(finalize(() => (this.loading = false)))
       .subscribe({
         next: (data: any) => {
