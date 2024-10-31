a999e152e8f4744433c93b78264b939eb6789414
Christian Badura <christian.badura@capgemini.com>
2024-10-31 11:46:36 +0100

    fix: create product detail use component and do init slot setup

---
+++ src/app/product-store/product-detail/product-use/product-use.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core'
+import { TranslateService } from '@ngx-translate/core'
+
+import { SlotService } from '@onecx/angular-remote-components'
+import { Observable } from 'rxjs'
+import { Product } from 'src/app/shared/generated'
+
+@Component({
+  selector: 'app-product-use',
+  templateUrl: './product-use.component.html'
+})
+export class ProductUseComponent {
+  @Input() product: Product | undefined
+  public isListWorkspacesUsingProductComponentDefined$: Observable<boolean> | undefined
+  public listWorkspacesUsingProductSlotName = 'onecx-product-list-workspaces-using-product'
+
+  constructor(
+    private readonly translate: TranslateService,
+    private readonly slotService: SlotService
+  ) {
+    this.isListWorkspacesUsingProductComponentDefined$ = this.slotService.isSomeComponentDefinedForSlot(
+      this.listWorkspacesUsingProductSlotName
+    )
+  }
+}
