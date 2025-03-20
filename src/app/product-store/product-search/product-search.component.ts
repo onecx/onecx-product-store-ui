f1a2a475e1fbe7c3e4b524da67141d8387411c1f
Henry Taeschner <github@henry-taeschner.de>
2025-03-20 12:39:07 +0100

    feat: added remote component for product-infos

--- src/app/product-store/product-search/product-search.component.ts
+++ src/app/product-store/product-search/product-search.component.ts
@@ -39,6 +39,7 @@ export class ProductSearchComponent implements OnInit {
   public filter: string | undefined
   public sortField = 'displayName'
   public sortOrder = 1
+  public quickFilterItems: string[] = []
 
   @ViewChild(DataView) dv: DataView | undefined
   public dataViewControlsTranslations$: Observable<DataViewControlTranslations> | undefined
@@ -77,7 +78,11 @@ export class ProductSearchComponent implements OnInit {
           const products: ProductAbstractExtended[] = []
           data.stream?.forEach((p) => {
             products.push({ ...p, classes: p.classifications?.join(', ') })
+            p.classifications?.forEach((c) => {
+              if (!this.quickFilterItems.includes(c)) this.quickFilterItems.push(c)
+            })
           })
+          console.log(this.quickFilterItems)
           return products.sort(this.sortProductsByDisplayName)
         }),
         catchError((err) => {
