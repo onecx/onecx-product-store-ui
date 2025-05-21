cb2bcc7421976e1679fc1e6fae58588d568fcf2c
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2025-05-21 07:49:20 +0200

    feat: use rc workspace data for usage detection (#439)
    * feat: use rc workspace data for usage detection
    
    * feat: disable log

--- src/app/product-store/product-detail/product-use/product-use.component.ts
+++ src/app/product-store/product-detail/product-use/product-use.component.ts
@@ -1,21 +1,47 @@
-import { Component, Input } from '@angular/core'
-import { Observable } from 'rxjs'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { BehaviorSubject, Observable } from 'rxjs'
 
 import { SlotService } from '@onecx/angular-remote-components'
 
-import { Product } from 'src/app/shared/generated'
+export type Workspace = {
+  name: string
+  displayName: string
+  description?: string
+  theme?: string
+  homePage?: string
+  baseUrl?: string
+  companyName?: string
+  phoneNumber?: string
+  rssFeedUrl?: string
+  footerLabel?: string
+  logoUrl?: string
+  mandatory?: boolean
+  operator?: boolean
+  disabled?: boolean
+}
 
 @Component({
   selector: 'app-product-use',
   templateUrl: './product-use.component.html'
 })
-export class ProductUseComponent {
-  @Input() product: Product | undefined
+export class ProductUseComponent implements OnInit {
+  @Input() productName: string | undefined
+  @Output() used = new EventEmitter<boolean>()
 
+  // receive the slot output
+  public slotName = 'onecx-workspace-data'
+  public slotEmitter = new EventEmitter<Workspace[]>()
+  public workspaceData$ = new BehaviorSubject<Workspace[] | undefined>(undefined)
   public isComponentDefined$: Observable<boolean> | undefined
-  public slotName = 'onecx-product-list-workspaces-using-product'
 
   constructor(private readonly slotService: SlotService) {
     this.isComponentDefined$ = this.slotService.isSomeComponentDefinedForSlot(this.slotName)
+  }
+
+  public ngOnInit(): void {
+    this.slotEmitter.subscribe((res) => {
+      this.workspaceData$.next(res)
+      if (res.length > 0) this.used.emit(true)
+    })
   }
 }
